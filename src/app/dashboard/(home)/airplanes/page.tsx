8eaa158ec3fd1afcd7c753d179cbc80044e1ec66
Dika <nardika.kurniawan@lumoshive.com>
2024-08-09 22:12:18 +0700

    feat: dashboard airplanse - create data | setup supabase storage for upload image

---
+++ src/app/dashboard/(home)/airplanes/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+import { getAirplanes } from './lib/data';
+import { DataTable } from '@/components/ui/data-table';
+import { Button } from '@/components/ui/button';
+import { columns } from './components/columns-table';
+
+export const metadata: Metadata = {
+  title: 'Dashboard | Airplanes',
+};
+
+export default async function AirplanePage() {
+  const planes = await getAirplanes();
+
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between">
+        <div className="my-5 text-2xl font-bold">Airplanes</div>
+        <Button asChild>
+          <Link href={'/dashboard/airplanes/create'}>
+            <Plus className="mr-2 h-4 w-4" />
+            Tambah Data
+          </Link>
+        </Button>
+      </div>
+      <DataTable columns={columns} data={planes} />
+    </>
+  );
+}
